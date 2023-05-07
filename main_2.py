1b13661bbd7050ef7a7bf043f7eee01f36611023
Raphael Simões <95584209+simoesxs@users.noreply.github.com>
2023-05-07 12:37:45 -0300

    Update main_2.py

--- main_2.py
+++ main_2.py
@@ -1,49 +1,47 @@
-pergunta = int(input("Voce quer ver o seu IMC ou a altura do triangulo?\n1 - Área do triangulo\n2 - IMC\n"))
+import os
+
+def clear():{
+    os.system('cls||clear')
+}
+clear()
+pergunta = int(input("Voce quer ver o seu IMC ou a altura do triangulo?\n1 - Área do triangulo\n2 - IMC\n>>>"))
+
+# Triangle/ Triângulo
 
 if pergunta == 1:
-    print("================================")
-    print("        Área do triangulo")
-    print("================================\n")
-
-    altura_triangulo = int(input("Qual a altura do seu triangulo? "))
-    print("================================\n")
-    base_triangulo = int(input("Qual a base do seu triangulo? "))
+    clear()
+    altura_triangulo = int(input("Qual a altura do seu triangulo?\n>>>"))
+    clear()
+    base_triangulo = int(input("Qual a base do seu triangulo?\n>>>"))
     print("================================\n")
     area= print(f"A área do seu triangulo é: {(base_triangulo * altura_triangulo)/2}\n")
 
+#IMC
+
 if pergunta == 2:
-    print("================================")
-    print("              IMC")
-    print("================================\n")
-    peso = float(input("Qual é o seu peso? "))
-    print("================================\n")
-    altura = float(input("Qual a sua altura? "))
-    print("================================\n")
-
+    clear()
+    peso = float(input("Qual é o seu peso?\n>>>"))
+    clear()
+    altura = float(input("Qual a sua altura?\n>>>"))
     imc = peso/(altura*altura)
-
-    area= print(f"Seu IMC é: {imc}\n\n")
+    clear()
+    area= print(f"Seu IMC é: {imc:.2f}\n\n")
 
     if imc < 18.5:
-            print("================================\n")
-            print("está a baixo do peso\n")
-
-    elif imc == 18.5 and imc < 24.9:
-            print("================================\n")
-            print("está com o peso normal\n")
-            
-    elif imc == 25.0 and imc < 29.9:
-            print("================================\n")
-            print("está com o sobrepeso\n")
-
-    elif imc == 30.0 and imc < 34.9:
-            print("================================\n")
-            print("está com obsidade tipo 1\n")
-
-    elif imc == 35.0 and imc < 39.9:
-            print("================================\n")
-            print("está com obsidade tipo 2\n")
-            
-    if imc > 40.0:
-            print("================================\n")
-            print("está com obsidade tipo 3\n")
+        print("================================\n")
+        print("está abaixo do peso\n")
+    elif imc < 24.9:
+        print("================================\n")
+        print("está com o peso normal\n")
+    elif imc < 29.9:
+        print("================================\n")
+        print("está com o sobrepeso\n")
+    elif imc < 34.9:
+        print("================================\n")
+        print("está com obsidade tipo 1\n")
+    elif imc < 39.9:
+        print("================================\n")
+        print("está com obsidade tipo 2\n")
+    else:
+        print("================================\n")
+        print("está com obsidade tipo 3\n")
